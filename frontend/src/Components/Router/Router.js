52883794fecdf242fd3e7232e6412ff967e5c711
Muhammad-haziq12 <you@example.com>
2022-11-20 17:05:34 +0100

    "routes organisation"

--- frontend/src/Components/Router/Router.js
+++ frontend/src/Components/Router/Router.js
@@ -1,4 +1,15 @@
-import routes from './routes';
+
+import GamePage from '../Pages/GamePage';
+import HomePage from '../Pages/HomePage';
+import NewPage from '../Pages/NewPage';
+import ProductPage from '../Pages/ProductPage';
+ 
+const routes = {
+  '/': HomePage,
+  '/game': GamePage,
+  '/new': NewPage,
+  '/product': ProductPage,
+};
 
 const Router = () => {
   onFrontendLoad();
