b76f17ccb38b07baa1cc90405d9b0770e6d841cb
Muhammad-haziq12 <you@example.com>
2022-11-20 16:37:03 +0100

    test1

--- frontend/src/Components/Game/ScoreLabel.js
+++ frontend/src/Components/Game/ScoreLabel.js
@@ -5,7 +5,7 @@ const formatScore = (score) => `Score: ${score}`;
 export default class ScoreLabel extends Phaser.GameObjects.Text {
   constructor(scene, x, y, score, style) {
     super(scene, x, y, formatScore(score), style);
-    console.log('inside class', this.text);
+   
     this.score = score;
   }
 
