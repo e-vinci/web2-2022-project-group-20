a850e4ce0146ee78487da1e721a038bbfe54813d
RedArms <reda_1030@hotmail.com>
2022-11-21 13:48:20 +0100

    article js

--- api/models/article.js
+++ api/models/article.js
@@ -3,10 +3,22 @@ const defaultarticle = {
     title: 'Default',
     price: 0,
     description: 'Default',
-    auteur: 'Default',
+    id_auteur: 0,
     images: ['https://www.google.com'],
     googlemaplink: 'Bruxelles',
     categorie: 'Default',
     date: 'Default',
     etat: 'Default'
 };
+
+router.get('/:id', (req, res) => {
+    console.log(`GET /article/${req.params.id}`);
+    const id = req.params.id;
+    const article = articles.find((article) => article.id === id);
+
+    res.json(article);
+  });
+
+
+
+
