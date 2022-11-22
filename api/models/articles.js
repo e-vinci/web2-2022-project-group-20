b37f98dc19b798eb31f24c1af206f1740d986b8d
RedArms <reda_1030@hotmail.com>
2022-11-22 22:01:21 +0100

    creating query sql

--- api/models/articles.js
+++ api/models/articles.js
@@ -21,8 +21,114 @@ class Articles{
             throw new Error("Error while getting all posts from the database.");
         }
     }
-}
 
+
+    async getArticle(id){
+        const query = `SELECT id_annonce,
+                              nom,
+                              description,
+                              id_acheteur,
+                              id_vendeur,
+                              date_pub,
+                              prix,
+                              status
+                       FROM vinced.posts
+                       WHERE id_annonce = ${id}`;
+        try {
+            const {rows} = await db.query(query);
+            return rows;
+        } catch (e) {
+            throw new Error("Error while getting this post from the database.");
+        }
+    }
+
+
+    async getArticlesByUser(id){
+        const query = `SELECT id_annonce,
+                                nom,
+                                description,
+                                id_acheteur,
+                                id_vendeur,
+                                date_pub,
+                                prix,
+                                status
+                        FROM vinced.posts
+                        WHERE id_vendeur = ${id}
+                        ORDER BY id_annonce DESC`;
+        try {
+            const {rows} = await db.query(query);
+            return rows;
+        } catch (e) {
+            throw new Error("Error while getting all posts from the database.");
+        }
+    }
+
+    async getFavoriteArticles(id){
+        const query = `SELECT id_annonce,
+                                nom,
+                                description,
+                                id_acheteur,
+                                id_vendeur,
+                                date_pub,
+                                prix,
+                                status
+                        FROM vinced.posts
+                        WHERE id_annonce IN (SELECT id_annonce
+                                                FROM vinced.favoris
+                                                WHERE id_user = ${id})
+                        ORDER BY id_annonce DESC`;
+
+        try {
+            const {rows} = await db.query(query);
+            return rows;
+        } catch (e) {
+            throw new Error("Error while getting all posts from the database.");
+        }
+    }
+
+    async getArticlesByCategory(id){
+        const query = `SELECT id_annonce,
+                                nom,
+                                description,
+                                id_acheteur,
+                                id_vendeur,
+                                date_pub,
+                                prix,
+                                status
+                        FROM vinced.posts
+                        WHERE id_categorie = ${id}
+                        ORDER BY id_annonce DESC`;
+        try {
+            const {rows} = await db.query(query);
+            return rows;
+        } catch (e) {
+            throw new Error("Error while getting all posts from the database.");
+        }
+    }
+
+    async getArticlesBySearch(search){
+        const query = `SELECT id_annonce,
+                                nom,
+                                description,
+                                id_acheteur,
+                                id_vendeur,
+                                date_pub,
+                                prix,
+                                status
+                        FROM vinced.posts
+                        WHERE nom ILIKE '%${search}%'
+                        ORDER BY id_annonce DESC`;
+        try {
+            const {rows} = await db.query(query);
+            return rows;
+        } catch (e) {
+            throw new Error("Error while getting all posts from the database.");
+        }
+    }
+
+    
+
+}
 module.exports = {Articles};
 // const defaultarticle = {
 //     id: 0,
