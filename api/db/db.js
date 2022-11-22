1f4473295d2ab36d18fa6de20dbfb766c8088c75
Victor Denis <victor.denis@student.vinci.com>
2022-11-22 22:05:11 +0100

    BACKEND : Fix : Now use local DB

--- api/db/db.js
+++ api/db/db.js
@@ -1,16 +1,22 @@
 const {Pool} = require("pg");
 
-const dbConfig = {
+
+/* const dbConfig = {
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     database: process.env.DB_DATABASE,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     ssl: {rejectUnauthorized: false}
-};
+}; */
 
-const pool = new Pool(dbConfig);
+// const pool = new Pool(dbConfig);
+
+const pool = new Pool({
+    connectionString: process.env.DB_URL,
+    ssl: !!process.env.DATABASE_URL
+})
 
 module.exports = {
-    query: (text, params) => pool.query(text, params)
+    query: (text, params) =>  pool.query(text, params)
 }
