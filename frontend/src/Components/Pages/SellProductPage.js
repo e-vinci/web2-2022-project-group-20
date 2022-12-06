0e2b6ae00fab9ab31342033aba9775d3842e9a41
RedArms <reda_1030@hotmail.com>
2022-12-06 11:16:59 +0100

    innit sell page

---
+++ frontend/src/Components/Pages/SellProductPage.js
@@ -0,0 +1,14 @@
+const SellProductPage = ()=>{
+    renderSellProductPage();
+
+};
+
+function renderSellProductPage(){
+    const render = ` `;
+
+  const main = document.querySelector("main");
+  main.innerHTML = render;
+
+}
+
+export default SellProductPage;
