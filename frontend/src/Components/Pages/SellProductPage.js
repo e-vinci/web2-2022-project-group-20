13719ebc85bdeae46da5d05acd06d2d7eb146c29
RedArms <reda_1030@hotmail.com>
2022-12-06 11:43:19 +0100

    sell product page fondation

--- frontend/src/Components/Pages/SellProductPage.js
+++ frontend/src/Components/Pages/SellProductPage.js
@@ -4,10 +4,58 @@ const SellProductPage = ()=>{
 };
 
 function renderSellProductPage(){
-    const render = ` `;
+    const render = `
+    <form>
+
+        <label for="name">Titre (min 4 chars):</label>
+        <input type="text" id="name" name="name" required minlength="4" size="30">
+
+        <p><label for="w3review">Description :</label></p>
+        <textarea id="description" name="description" rows="4" cols="50"></textarea>
+
+        <label for="name">Prix :</label>
+        <input type="text" id="prix" name="prix" required minlength="4" size="5">
+        
+        <br>
+
+        <label for="name">Photo :</label>
+        <input type="file" id="file" name="image" accept="image/*">
+
+        <input type="hidden" id="id_vendeur" name="id_vendeur" value="${window.localStorage.getItem("user")}" />
+
+
+        <br>
+        <br>
+        <button type="submit" class="btn btn-primary btn-lg">En vente !</button>
+
+
+    </form>
+        
+          `;
 
   const main = document.querySelector("main");
   main.innerHTML = render;
+
+
+  const file = document .getElementById( "file")
+  const img = document. getElementById( "img")
+  const url=document . getElementById( "url")
+  file.addEventListener("change", ev => {
+  const formdata = new FormData()
+  formdata.append( "image", ev.target.files[0])
+    fetch ( "https://api.imgur.com/3/image/" ,{
+    method: "post" ,
+    headers: {
+        Authorization: "Client- ID"}
+        ,body: formdata
+    }).then(data => data.json()).then(data => {
+        img.src=data.data.link
+        url.innerText=data.data.link
+    })  
+})
+
+  main.innerHTML = render;
+
 
 }
 
