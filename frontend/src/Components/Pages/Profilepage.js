1ed3819273b2f3ac43b5efa80cfb0d01a68788ec
Navial <victor.denis@gmail.com>
2022-12-12 14:42:19 +0100

    Front: implementation profile

--- frontend/src/Components/Pages/Profilepage.js
+++ frontend/src/Components/Pages/Profilepage.js
@@ -22,7 +22,7 @@ import gameboyimg from '../../img/gameboy.png';
     response = await response.json();
     const member = response[0];
 
-    const profilepage= `
+    let profilepage= `
     <section style="background-color: #eee;">
     <div class="containerpanier">
       <div class="row">
@@ -31,37 +31,9 @@ import gameboyimg from '../../img/gameboy.png';
             <div class="card-body text-center">
               <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava3.webp" alt="avatar"
                 class="rounded-circle img-fluid" style="width: 150px;">
-              <h5 class="my-3" >${member.nom}</h5>
-              <p class="text-muted mb-1">STUDENT</p>
-              <p class="text-muted mb-4">BRUXELLES</p>
-
+              <h5 class="my-3" >${member.prenom} ${member.nom}</h5>
             </div>
           </div>
-          <div class="card mb-4 mb-lg-0">
-            <div class="card-body p-0">
-              <ul class="list-group list-group-flush rounded-3">
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fas fa-globe fa-lg text-warning"></i>
-                  <p class="mb-0">https://mdbootstrap.com</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-github fa-lg" style="color: #333333;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-twitter fa-lg" style="color: #55acee;"></i>
-                  <p class="mb-0">@mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-instagram fa-lg" style="color: #ac2bac;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-                <li class="list-group-item d-flex justify-content-between align-items-center p-3">
-                  <i class="fab fa-facebook-f fa-lg" style="color: #3b5998;"></i>
-                  <p class="mb-0">mdbootstrap</p>
-                </li>
-              </ul>
-            </div>
           </div>
         </div>
         <div class="col-lg-8">
@@ -72,7 +44,7 @@ import gameboyimg from '../../img/gameboy.png';
                   <p class="mb-0">Full Name</p>
                 </div>
                 <div class="col-sm-9">
-                  <p class="text-muted mb-0">MUHAMMAD HAZIQ</p>
+                  <p class="text-muted mb-0">${member.prenom} ${member.nom}</p>
                 </div>
               </div>
               <hr>
@@ -81,10 +53,10 @@ import gameboyimg from '../../img/gameboy.png';
                   <p class="mb-0">Email</p>
                 </div>
                 <div class="col-sm-9">
-                  <p class="text-muted mb-0">${member.mail}</p>
+                  <p class="text-muted mb-0">${member.email}</p>
                 </div>
               </div>
-              <hr>
+              <!-- <hr>
               <div class="row">
                 <div class="col-sm-3">
                   <p class="mb-0">Phone</p>
@@ -110,13 +82,11 @@ import gameboyimg from '../../img/gameboy.png';
                 <div class="col-sm-9">
                   <p class="text-muted mb-0"></p>
                 </div>
-              </div>
+              </div>-->
             </div>
-          </div>
+          </div>`
 
-          
-
-            <div class="row">
+          profilepage += `<div class="row">
             <div class="col-md-6">
               <div class="card mb-4 mb-md-0">
                 <div class="card-body">
