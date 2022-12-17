96af04a2832668ac43b165902b65bd5b8faddf2a
Navial <victor.denis@gmail.com>
2022-12-18 00:24:31 +0100

    Fix: navbar

--- frontend/src/Components/Navbar/Navbar.js
+++ frontend/src/Components/Navbar/Navbar.js
@@ -10,7 +10,7 @@ import logo from '../../img/logo.png';
 
 const Navbar = () => {
   renderNavbar();
-  // darkmode();
+  darkmode();
 };
 
 async function renderNavbar() {
@@ -102,11 +102,21 @@ async function renderNavbar() {
         </li>
       `;
     }
+  } else {
+    navbarHtml += ` </ul></div>
+    <div class="bottom-content">
+    <li >
+        <a href="/loginPage"  class="logBtn" id="login">
+            <i class='bx bx-log-in icon' ></i>
+            <span class="text nav-text">LOG IN </span>
+        </a>
+    </li>
+`;
   }
 
   navbarHtml += `
 
-    <!-- <li class="mode">
+    <li class="mode">
         <div class="sun-moon">
             <i class='bx bx-moon icon moon'></i>
             <i class='bx bx-sun icon sun'></i>
@@ -116,7 +126,7 @@ async function renderNavbar() {
         <div class="toggle-switch">
             <span class="switch"></span>
         </div>
-    </li>--> 
+    </li>
     
 </div>
 </div>
@@ -187,19 +197,19 @@ Find the connected member and retrieve it
   });
 } */
 
-// function darkmode() {
-//   const body = document.querySelector('body');
-//   const modeSwitch = body.querySelector('.toggle-switch');
-//   const modeText = body.querySelector('.mode-text');
+function darkmode() {
+  const body = document.querySelector('body');
+  const modeSwitch = body.querySelector('.toggle-switch');
+  const modeText = body.querySelector('.mode-text');
 
-//   modeSwitch.addEventListener('click', () => {
-//     body.classList.toggle('dark');
-//     if (body.classList.contains('dark')) {
-//       modeText.innerText = 'Light mode';
-//     } else {
-//       modeText.innerText = 'Dark mode';
-//     }
-//   });
-// }
+  modeSwitch.addEventListener('click', () => {
+    body.classList.toggle('dark');
+    if (body.classList.contains('dark')) {
+      modeText.innerText = 'Light mode';
+    } else {
+      modeText.innerText = 'Dark mode';
+    }
+  });
+}
 
 export default Navbar;
